baac7fc030970f728d3662e667f9f5b3f1eb2174
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-07-20 02:44:38 +0300

    add participantsServices

--- services/participantsServices.js
+++ services/participantsServices.js
@@ -1,12 +1,13 @@
 import { Participant } from "../models/Participant.js";
 
-export const listParticipants = async (filter = {}, query = {}) => {
-  const events = await Participant.find(
-    filter,
-    "title description event_date organizer logo_url",
-    query
-  );
-  const total = await Event.countDocuments();
+export const listParticipants = async (filter = {}) => {
+  const participants = await Participant.find(filter);
 
-  return { events, total };
+  return participants;
+};
+
+export const createParticipant = async (data) => {
+  const newParticipant = await Participant.create(data);
+
+  return newParticipant;
 };
