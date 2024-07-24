310e06340c1a5a1f515939155214bd6358d5db3f
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-07-24 16:36:48 +0300

    add sort events

--- controllers/eventsControllers.js
+++ controllers/eventsControllers.js
@@ -2,9 +2,20 @@ import { ctrlWrapper, HttpError } from "../helpers/index.js";
 import { getEventById, listEvents } from "../services/eventsServices.js";
 
 const getAllEvents = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, byDate, byTitle, byOrganizer } = req.query;
   const skip = (page - 1) * limit;
-  const result = await listEvents({}, { skip, limit });
+  const sort = {};
+  console.log("skip", skip);
+  if (byDate !== undefined) {
+    sort.event_date = byDate === "true" ? 1 : -1;
+  }
+  if (byTitle !== undefined) {
+    sort.title = byTitle === "true" ? 1 : -1;
+  }
+  if (byOrganizer !== undefined) {
+    sort.organizer = byOrganizer === "true" ? 1 : -1;
+  }
+  const result = await listEvents({}, { skip, limit }, sort);
 
   res.json(result);
 };
--- services/eventsServices.js
+++ services/eventsServices.js
@@ -1,13 +1,14 @@
 import { Event } from "../models/Event.js";
 
-export const listEvents = async (filter = {}, query = {}) => {
+export const listEvents = async (filter = {}, query = {}, sort = null) => {
   const events = await Event.find(
     filter,
     "title description event_date organizer logo_url",
     query
-  );
-  const total = await Event.countDocuments();
-
+  )
+    .sort(sort)
+    .exec();
+  const total = await Event.countDocuments(filter);
   return { events, total };
 };
 
