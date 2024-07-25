2ab43d0d345202220fff5a7e074ec7bbf2098f54
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-07-25 19:05:28 +0300

    add model User, authenticate middelwares and usersSchemas

---
+++ schemas/usersSchemas.js
@@ -0,0 +1,18 @@
+import Joi from "joi";
+
+import { emailRegExp } from "../constants/emailRegExp.js";
+
+export const registerUserSchema = Joi.object({
+  name: Joi.string().min(2).required(),
+  password: Joi.string().min(6).required(),
+  email: Joi.string().pattern(emailRegExp).required(),
+});
+
+export const emailUserSchema = Joi.object({
+  email: Joi.string().pattern(emailRegExp).required(),
+});
+
+export const loginUserSchema = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().pattern(emailRegExp).required(),
+});
